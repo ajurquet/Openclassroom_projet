01f44301ad9b9c2659395e3cd40dec6c51f8bffd
Alexandre Jurquet <a.jurquet@gmail.com>
2021-03-14 10:17:02 +0100

    essai csv

--- essai_scrap.py
+++ essai_scrap.py
@@ -1,23 +1,42 @@
-
-
 from bs4 import BeautifulSoup
 import requests
+import os
 import csv
 
+"""
+Maintenant que vous avez obtenu les informations concernant un premier livre, vous pouvez essayer de récupérer 
+toutes les données nécessaires pour toute une catégorie d'ouvrages. Choisissez n'importe quelle catégorie sur 
+le site de Books to Scrape. Écrivez un script Python qui consulte la page de la catégorie choisie, et extrait 
+l'URL de la page Produit de chaque livre appartenant à cette catégorie. Combinez cela avec le travail que vous 
+avez déjà effectué afin d'extraire les données produit de tous les livres de la catégorie choisie, puis écrivez 
+les données dans un seul fichier CSV.
 
-url = "http://books.toscrape.com/index.html"
+Remarque : certaines pages de catégorie comptent plus de 20 livres, qui sont donc répartis sur différentes 
+pages («  pagination  »). Votre application doit être capable de parcourir automatiquement les multiples pages si présentes. 
+"""
+
+liste_urls_livres = []
+url = "http://books.toscrape.com/catalogue/category/books/historical-fiction_4/index.html"
 req = requests.get(url)
+soup = BeautifulSoup(req.text, features="html.parser")
 
-# soup = BeautifulSoup(req.text, features="html.parser")
-# soup = soup.find("ul", {"class" : "nav"}).find("li").text.replace("\n", "") # Trouve les strings des catégories, pour colonnes dans CSV
-# categorie = []
-# for i in categorie:
-#     categorie += categorie[i]
-#
-# print(categorie)
+urls_livre = soup.findAll("h3")
+for i in urls_livre: # boucle qui copie chaque url de la page dans une liste
+    lien = i.find("a")
+    lien = lien["href"]
+    lien = lien.replace("../../..", "http://books.toscrape.com/catalogue")
+    liste_urls_livres.append((lien))
+    # print(liste_urls_livres)
+
+# noms_cat = soup.find("ul", {"class" : "nav"}).find("li").text.replace("\n", "") # Trouve les strings des catégories, pour colonnes dans CSV
+# print(noms_cat.replace())
+
+# nom_cat = soup.find("h1").text
 
 
-def scrap_page_livre(url):
+
+
+def scrap_page_livre(url_page_livre):
     """
     Écrivez un script Python qui visite cette page et en extrait les informations suivantes :
     product_page_url
@@ -33,8 +52,8 @@ def scrap_page_livre(url):
     Écrivez les données dans un fichier CSV qui utilise les champs ci-dessus comme en-têtes de colonnes.
     """
     #url de la page du livre à scrapper
-    url_livre = "http://books.toscrape.com/catalogue/i-know-what-im-doing-and-other-lies-i-tell-myself-dispatches-from-a-life-under-construction_704/index.html"
-    req = requests.get(url_livre)
+    #url_livre = "http://books.toscrape.com/catalogue/i-know-what-im-doing-and-other-lies-i-tell-myself-dispatches-from-a-life-under-construction_704/index.html"
+    req = requests.get(url_page_livre)
 
     if req.ok:
         soup = BeautifulSoup(req.text, features="html.parser")
@@ -51,10 +70,19 @@ def scrap_page_livre(url):
 
         image_source = soup.find("img").get("src")
 
-        with open("fiche_livre.csv", "w", encoding="utf-8") as livre_csv:
+        with open(titre.text.replace(":", " ") + ".csv", "w", encoding="utf-8") as livre_csv:
             livre_csv.write("product_page_url, universal_ product_code (upc), title, price_including_tax,"
                             "price_excluding_tax, number_available, product_description, category, review_rating, image_url" "\n\n")
-            livre_csv.write(url_livre + ",\n" + liste_carac_livre[0].text + ",\n" + titre.string + ",\n" +
+            livre_csv.write(url_page_livre + ",\n" + liste_carac_livre[0].text + ",\n" + titre.string + ",\n" +
                             liste_carac_livre[3].text.replace("Â", "") + ",\n" + liste_carac_livre[2].text.replace("Â", "") +
                             ",\n" + liste_carac_livre[5].text + ",\n" + '"' + description + '"' + ",\n" + categorie.text.replace("\n","") +
                             ",\n" + liste_carac_livre[6].text + ",\n" + image_source.replace("../..", "http://books.toscrape.com"))
+
+
+csv = scrap_page_livre("http://books.toscrape.com/catalogue/forever-and-forever-the-courtship-of-henry-longfellow-and-fanny-appleton_894/index.html")
+print(csv)
+
+# for i in range(len(liste_urls_livres)):
+#     scrap_page_livre(liste_urls_livres[i])
+
+
