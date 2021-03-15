aec0fb12644e32dd277bddf14448d90f6bbb69f6
Alexandre Jurquet <a.jurquet@gmail.com>
2021-03-15 12:44:28 +0100

    Ajout des fichiers P2_02

---
+++ P2_02_Scrap_categorie.py
@@ -0,0 +1,87 @@
+"""
+Maintenant que vous avez obtenu les informations concernant un premier livre, vous pouvez essayer de récupérer toutes
+les données nécessaires pour toute une catégorie d'ouvrages. Choisissez n'importe quelle catégorie sur le site de
+Books to Scrape. Écrivez un script Python qui consulte la page de la catégorie choisie, et extrait l'URL de la
+page Produit de chaque livre appartenant à cette catégorie. Combinez cela avec le travail que vous avez déjà effectué
+afin d'extraire les données produit de tous les livres de la catégorie choisie, puis écrivez les données
+dans un seul fichier CSV.
+
+Remarque : certaines pages de catégorie comptent plus de 20 livres, qui sont donc répartis sur différentes pages
+(«  pagination  »). Votre application doit être capable de parcourir automatiquement les multiples pages si présentes.
+"""
+
+from bs4 import BeautifulSoup
+import requests
+
+url_cat = "http://books.toscrape.com/catalogue/category/books/sequential-art_5/index.html"
+liste_urls_livres = []
+fin_url = "index.html"
+
+
+def copie_urls_livre(url_a_parcourir):
+    """
+    Fonction qui parcours l'url de la catégorie donnée en paramètre,
+    et copie les liens vers chaque page "livre" dans la liste "liste_urls_livres"
+    """
+    global fin_url
+    req = requests.get(url_a_parcourir)
+    soup = BeautifulSoup(req.text, features="html.parser")
+    url_boucle = soup.findAll("h3")
+    url_courte = url_a_parcourir.replace(fin_url, "")
+
+    for i in url_boucle: #parcours l'url et copie tous les liens pointant vers une page "livre" dans une liste
+        lien = i.find("a")
+        lien = lien["href"]
+        lien = lien.replace("../../..", "http://books.toscrape.com/catalogue")
+        liste_urls_livres.append((lien))
+
+    if soup.find("li", {"class": "next"}): # si il y a un bouton "next sur la page, relance la fonction avec la page suivante
+        bouton_next = soup.find("li", {"class": "next"})
+        url_next_page = bouton_next.find("a")
+        url_next_page = url_next_page["href"]
+        fin_url = url_next_page
+        url_next_page = url_courte + url_next_page
+        copie_urls_livre(url_next_page)
+
+
+def scrap_page_livre(url_page_livre):
+    """
+    Fonction qui visite une page "livre" et en extrait des informations
+
+    """
+    req = requests.get(url_page_livre)
+
+    if req.ok:
+        soup = BeautifulSoup(req.text, features="html.parser")
+
+        titre = soup.find("h1")
+
+        liste_carac_livre = soup.findAll("td")
+
+        description = soup.find("div", id="product_description")
+        description = description.nextSibling.nextSibling.string # J'ai recherché le tag "div" avec l'id "product_description" et je me suis déplacé 2 fois pour trouver la description
+
+        categorie = soup.find("li")
+        categorie = categorie.nextSibling.nextSibling.nextSibling.nextSibling
+
+        image_source = soup.find("img").get("src")
+
+        # donne en résultat les informations demandées, séparées par des virgules.
+        return (url_page_livre + "," + '"' + liste_carac_livre[0].text + '"' + "," + '"' + titre.string + '"' + "," + '"' +
+                            liste_carac_livre[3].text.replace("Â", "") + '"' + "," + '"' + liste_carac_livre[2].text.replace("Â", "") + '"' +
+                            "," + '"' + liste_carac_livre[5].text + '"' + "," + '"' + description.replace('"', '^') + '"' + "," + '"' + categorie.text.replace("\n","") + '"' +
+                            "," + '"' + liste_carac_livre[6].text + '"' + "," +  image_source.replace("../..", "http://books.toscrape.com") + "\n")
+
+
+# Trouve le texte de la catégorie pour nom du fichier csv
+req = requests.get(url_cat)
+if req.ok:
+    soup = BeautifulSoup(req.text, features="html.parser")
+    nom_fichier_csv = soup.find("h1").text
+
+copie_urls_livre(url_cat)
+with open(nom_fichier_csv.lower().replace(" ", "_") + ".csv", "w", encoding="utf-8") as cat_csv:
+    cat_csv.write("product_page_url, universal_ product_code (upc), title, price_including_tax,"
+                    "price_excluding_tax, number_available, product_description, category, review_rating, image_url" "\n\n")
+    for url in liste_urls_livres:
+        cat_csv.write(scrap_page_livre(url))
--- essai_scrap.py
+++ essai_scrap.py
@@ -1,42 +1,48 @@
 from bs4 import BeautifulSoup
 import requests
-import os
-import csv
+
 
 liste_urls_livres = []
+liste_noms_categories = []
+fin_url = "index.html"
 
-"""
-Maintenant que vous avez obtenu les informations concernant un premier livre, vous pouvez essayer de récupérer 
-toutes les données nécessaires pour toute une catégorie d'ouvrages. Choisissez n'importe quelle catégorie sur 
-le site de Books to Scrape. Écrivez un script Python qui consulte la page de la catégorie choisie, et extrait 
-l'URL de la page Produit de chaque livre appartenant à cette catégorie. Combinez cela avec le travail que vous 
-avez déjà effectué afin d'extraire les données produit de tous les livres de la catégorie choisie, puis écrivez 
-les données dans un seul fichier CSV.
 
-Remarque : certaines pages de catégorie comptent plus de 20 livres, qui sont donc répartis sur différentes 
-pages («  pagination  »). Votre application doit être capable de parcourir automatiquement les multiples pages si présentes. 
-"""
+# Copie toutes les urls des catégories dans une liste
+url_de_base = "http://books.toscrape.com/"
+req = requests.get(url_de_base)
+soup = BeautifulSoup(req.text, features="html.parser")
+cats = soup.find("ul", class_="nav").findAll("a")
+for i in cats:
+    i = i["href"]
+    i = url_de_base + i
+    liste_noms_categories.append([i])
+del(liste_noms_categories[0])
+
 
 def copie_urls_liste(url_a_parcourir):
     """
     Parcours l'url donnée en paramètre, et copie les liens vers chaque page dans une liste
-
     """
+    global fin_url
     req = requests.get(url_a_parcourir)
     soup = BeautifulSoup(req.text, features="html.parser")
-
     url_boucle = soup.findAll("h3")
-    for i in url_boucle:
+    url_courte = url_a_parcourir.replace(fin_url, "")
+
+    for i in url_boucle: #parcours l'url et copie tous les liens pointant vers une page "livre" dans une liste
         lien = i.find("a")
         lien = lien["href"]
         lien = lien.replace("../../..", "http://books.toscrape.com/catalogue")
         liste_urls_livres.append((lien))
 
-    if soup.find("li", {"class": "next"}): # si il y a un bouton "next sur la page
+    if soup.find("li", {"class": "next"}): # si il y a un bouton "next sur la page, relance la fonction avec la page suivante
+        # if fin_url != "":
+        #     url_courte = url_courte - fin_url
         bouton_next = soup.find("li", {"class": "next"})
         url_next_page = bouton_next.find("a")
         url_next_page = url_next_page["href"]
-        url_next_page = url_a_parcourir.replace("index.html", url_next_page)
+        fin_url = url_next_page
+        url_next_page = url_courte + url_next_page
         copie_urls_liste(url_next_page)
 
 
@@ -55,8 +61,6 @@ def scrap_page_livre(url_page_livre):
     image_url
     Écrivez les données dans un fichier CSV qui utilise les champs ci-dessus comme en-têtes de colonnes.
     """
-    #url de la page du livre à scrapper
-    #url_livre = "http://books.toscrape.com/catalogue/i-know-what-im-doing-and-other-lies-i-tell-myself-dispatches-from-a-life-under-construction_704/index.html"
     req = requests.get(url_page_livre)
 
     if req.ok:
@@ -83,8 +87,10 @@ def scrap_page_livre(url_page_livre):
                             "," + liste_carac_livre[6].text + "," + image_source.replace("../..", "http://books.toscrape.com"))
 
 
-copie_urls_liste("http://books.toscrape.com/catalogue/category/books/historical-fiction_4/index.html")
+copie_urls_liste("http://books.toscrape.com/catalogue/category/books/nonfiction_13/index.html")
 print(liste_urls_livres)
+
+
 
 
 
